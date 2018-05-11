681b6a6b1e3a3510613ca5bf6e237287b630f14f
Trey Morrison <treymorrison94@gmail.com>
2018-05-11 00:29:59 -0400

    Bethesda

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+    name: "Dragonborn",
+    photo: "https://vignette.wikia.nocookie.net/elderscrolls/images/4/4f/Dovahkiin_%28dragonborn%29.jpg/revision/latest?cb=20110831192724",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Lone Wanderer",
+    photo: "https://vignette.wikia.nocookie.net/fallout/images/2/2e/Lone_Wanderer.jpg/revision/latest?cb=20150510133635",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Courier 6",
+    photo: "https://vignette.wikia.nocookie.net/fallout/images/4/49/LR_Courier.jpg/revision/latest?cb=20120625052902",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Sole Survivor",
+    photo: "https://vignette.wikia.nocookie.net/fallout/images/a/ac/Sole_Survivor.png/revision/latest?cb=20151108062748",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Chosen One",
+    photo: "https://vignette.wikia.nocookie.net/fallout/images/7/7d/Chosen_One_Car.jpg/revision/latest?cb=20090720172204",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Vault Dweller",
+    photo: "https://vignette.wikia.nocookie.net/fallout/images/c/cd/Into_the_desert.jpg/revision/latest?cb=20121028182201",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
